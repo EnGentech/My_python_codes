aad1b41e03aa9fa2522e6938a9869790179d857d
EnGentech <engen.inyang@gmail.com>
2023-06-01 19:12:34 +0100

    added

--- sales_shop.py
+++ sales_shop.py
@@ -1,5 +1,8 @@
+import os
+import sys
 class sales_shop:
-
+    lis = ["Meat_pie", "Juice", "Ice_cream"]
+    option = ["Query"]
     def __int__(self):
         self.ss = ""
         self.ms = ""
@@ -32,21 +35,69 @@ class sales_shop:
         ice = sales_shop()
         ice.Ice_cream()
 
-        dic = {"Ice_cream": [ju.qty_ss, ju.qty_ms],
+        dic = {"Ice_cream": [ice.qty_ss, ice.qty_ms],
                "Meat_pie": [mit.qty_ss, mit.qty_ms],
-               "Juice": [ice.qty_ss, ice.qty_ms]
+               "Juice": [ju.qty_ss, ju.qty_ms]
                }
-        if prod == "all":
+
+        if prod == "All":
             for keys, values in dic.items():
                 print("{} remains: Small size {}, Medium size {}".format(keys, values[0], values[1]))
-        for keys, values in dic.items():
-            if prod == keys:
-                for i in range(len(values)):
-                    while i == 0:
-                        print("Small-size remains {}.".format(values[0]))
-                        break
-                    while i == 1:
-                        print("Medium-size remains {}.".format(values[1]))
-                        break
-b = sales_shop()
-b.Querry("Juice")
+        elif not prod or prod not in dic:
+            print("Invalid Querry option")
+        else:
+            for keys, values in dic.items():
+                if prod == keys:
+                    for i in range(len(values)):
+                        while i == 0:
+                            print("Small-size remains {}.".format(values[0]))
+                            break
+                        while i == 1:
+                            print("Medium-size remains {}.".format(values[1]))
+                            break
+
+
+def rem(inp):
+    b = sales_shop()
+    b.Querry(inp.capitalize())
+
+
+def check(*args):
+    if args[0].capitalize() in sales_shop().option:
+        if args[1].capitalize() in sales_shop().lis:
+            rem(arg[1])
+    else:
+        loop(arg[0])
+
+
+print("1 : Customer\n2 : Seller")
+quad = int(input("\nSelect 1 if you are a customer, or 2 if you are a seller\t\t"))
+arg = ""
+while True:
+    if quad == 1 or quad == 2:
+        break
+    else:
+        quad = int(input("Invalid selection, try again: "))
+
+if quad == 1:
+    print("to be carried out later. bye for now")
+    sys.exit()
+
+elif quad == 2:
+    print("\nwelcome, to query, please key in the command Query with the product\ne.g Query Juice\n")
+    command = input("Command$ ")
+    arg = command.split(" ")
+    check(arg[0], arg[1])
+
+
+def loop(valid):
+    while True:
+        if valid.capitalize() not in sales_shop().option:
+            print("Invalid Product, try again:")
+            law = input("Command$ ")
+            
+
+
+    else:
+        print("Invalid Command, try again:")
+        command = input("Command$ ")
